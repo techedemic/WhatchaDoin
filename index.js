c95ffbb1a658f8caeedddedcb8604d5b2d1abbba
Hendri Schoeman <Hendri Schoeman>
2017-06-20 10:09:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    //document.getElementById('lblTime').innerText = date + '-' + time;
+
+}, false);
